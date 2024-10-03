54d54b0352956ef70fb1d75b499c5e195ae8ef24
LibirSoft <libirsoft@gmail.com>
2024-10-03 18:35:40 +0300

    websocet

---
+++ src/middleWare/auth/WSAuthMiddleware.ts
@@ -0,0 +1,47 @@
+import { Inject, Middleware, type Context, type MiddlewareService } from '@asenajs/asena';
+import { UserService } from '../../core/service/UserService';
+import type { Next } from 'hono';
+import { Cookie_secret, Token_secret } from '../../env';
+import { ClientErrorStatusCode } from '@asenajs/asena/dist/lib/server/types';
+import { HTTPException } from 'hono/http-exception';
+import {Jwt} from "hono/utils/jwt";
+
+
+@Middleware()
+export class WSAuthMiddleware implements MiddlewareService {
+
+  @Inject(UserService)
+  private userService: UserService;
+
+  public async handle(context: Context, next: Next) {
+    const cookie = await context.getCookie('token', Cookie_secret);
+    const response = context.send('Unauthorized');
+
+    if (!cookie) {
+      console.log('no cookie');
+
+      throw new HTTPException(ClientErrorStatusCode.UNAUTHORIZED, { res: response as Response });
+    }
+
+    let user: any;
+
+    try {
+      const verified = await Jwt.verify(cookie, Token_secret);
+
+      console.log('ver', verified);
+
+      user = await this.userService.getUserById(verified['id'] as number);
+    } catch (e) {
+      throw new HTTPException(ClientErrorStatusCode.UNAUTHORIZED, { res: response as Response });
+    }
+
+    if (!user) {
+      throw new HTTPException(ClientErrorStatusCode.UNAUTHORIZED, { res: response as Response });
+    }
+
+    context.setWebSocketValue({ userName: user.userName, id: user.id });
+
+    await next();
+  }
+
+}
---
+++ src/websocket/TestWebSocket.ts
@@ -0,0 +1,31 @@
+import { AsenaWebSocketService, type Socket, WebSocket } from '@asenajs/asena';
+import type { User } from '../core/entitiy/User.ts';
+
+@WebSocket({ path: 'chat', middlewares: [], name: 'TestWebSocket' })
+export class TestWebSocket extends AsenaWebSocketService<{ user: User }> {
+
+  public onMessage(ws: Socket, message: Buffer | string) {
+    console.log('WebSocket sent a message:', message.toString());
+
+    ws.publishText('room', message.toString());
+
+    console.log('Message published to room');
+  }
+
+  public onClose(ws: Socket, code: number, reason: string) {
+    console.log('WebSocket closed');
+
+    console.log('Code:', code);
+
+    console.log('Reason:', reason);
+
+    console.log(ws.data ? ws.data.id : 'No data');
+  }
+
+  public onOpen(ws: Socket) {
+    console.log('websocket opened');
+
+    ws.subscribe('room');
+  }
+
+}
